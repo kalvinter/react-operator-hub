ca39ed897a1331ff80557a91ca1f1ae81f078e74
Kalvinter <web@blue-dots.at>
2023-09-23 21:29:06 +0200

    refactoring events

--- src/Events.js
+++ src/Events.js
@@ -13,7 +13,6 @@ export const noEventText = "Electricity Demand is stable."
 
 let decreaseEvents = [
     {
-        id: "D1",
         title: "High wind energy output",
         textStart: "Strong winds increase electricity generated from wind farms. Medium demand decrease expected shortly!",
         textEnd: "Winds have returned to normal. Medium demand decrease expected shortly!",
@@ -21,7 +20,6 @@ let decreaseEvents = [
         direction: effectDirection.decrease,
     },
     {
-        id: "D2",
         title: "High solar energy output",
         textStart: "A cloudless sky and strong sun increase electricity generated from solar energy farms. Low demand decrease expected shortly!",
         textEnd: "Solar output decreased again. Low demand decrease expected shortly!",
@@ -32,7 +30,6 @@ let decreaseEvents = [
 
 let increaseEvents = [
     {
-        id: "I1",
         title: "Reactor #7 is down",
         textStart: "Technical difficulties at reactor #7. Large demand surge expected shortly!",
         textEnd: "Reactor #7 will soon be back online. Large demand decrease expected shortly!",
@@ -40,7 +37,6 @@ let increaseEvents = [
         direction: effectDirection.increase,
     },
     {
-        id: "I2",
         title: "High demand from industry",
         textStart: "Temporary double-shifts were introduced at the industrial center. Medium demand surge expected shortly!",
         textEnd: "Temporary double-shifts at the industrial center ended. Medium demand decrease expected shortly!",
@@ -48,7 +44,6 @@ let increaseEvents = [
         direction: effectDirection.increase,
     },
     {
-        id: "I3",
         title: "New LLM app launched",
         textStart: "A new LLM wrapper app was launched and everyone is hyped. Medium demand surge by our data centers expected shortly!",
         textEnd: "People realised that the new app was not so good. Medium demand decrease expected shortly!",
@@ -56,7 +51,6 @@ let increaseEvents = [
         direction: effectDirection.increase,
     },
     {
-        id: "I4",
         title: "New Netflix series",
         textStart: "Everybody is about to watch the new series on Netflix. Low demand surge expected shortly!",
         textEnd: "Most people have finished wathcing the new episode. Low demand decrease decrease shortly!",
@@ -70,7 +64,16 @@ let increaseEvents = [
 export class AvailableEventHandler {
     constructor() {
         this.increaseEvents = increaseEvents
+
+        this.increaseEvents.forEach((element, index) => {
+            element.id = `I${index}`
+        })
+
         this.decreaseEvents = decreaseEvents
+
+        this.decreaseEvents.forEach((element, index) => {
+            element.id = `D${index}`
+        })
     }
 
     getAvailableEvents(direction){
