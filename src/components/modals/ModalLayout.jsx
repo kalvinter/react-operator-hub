18c94995c8559faecc13c2ddeea043217fd1c120
Kalvinter <web@blue-dots.at>
2023-10-04 19:28:50 +0200

    improve modal mobile version

--- src/components/modals/ModalLayout.jsx
+++ src/components/modals/ModalLayout.jsx
@@ -7,9 +7,9 @@ export default function ModalLayout(props) {
                 <div
                     className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                 >
-                    <div className="modal--content relative w-auto my-6 mx-auto max-w-3xl">
+                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
                     {/*content*/}
-                        <div className="shadow-lg relative flex flex-col w-full bg-neutral-600 border-solid border-2 border-gray-900 outline-none focus:outline-none">
+                        <div className="modal--content shadow-lg relative flex flex-col w-full bg-neutral-600 border-solid border-2 border-gray-900 outline-none focus:outline-none">
                             {/*header*/}
                             <div className="flex items-start justify-between p-5 border-b-2 border-solid border-gray-900 rounded-t">
                                 <h3 className="text-3xl font-semibold">
