7ab31cabc80673c4817c6d961e1418aa71d45edd
Kalvinter <web@blue-dots.at>
2023-11-26 20:19:04 +0100

    fix: visual fixes and added new logo svg

--- src/components/HubBanner.jsx
+++ src/components/HubBanner.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next';
-
-import { CubeTransparentIcon } from '@heroicons/react/20/solid'
+import Logo from '../assets/svg/logo.svg?react'
 
 function HubBanner() {
     const {t} = useTranslation()
@@ -9,10 +8,14 @@ function HubBanner() {
     return (
         <div className="p-2 text-color--hover rounded">
             <div className="mb-4">
-                <h1 className="justify-center md:justify-start mb-1 text-4xl flex items-center">
-                    <CubeTransparentIcon className="large-icon mr-2" /> OperatorHub <sup className="text-sm">TM</sup>
-                </h1>
+                
+                <div className="justify-center md:justify-start flex items-center">
+                    <Logo className="mr-2 w-[4rem] h-[4rem] main-logo-svg" /> 
+                    <h1 className="text-4xl">OperatorHub</h1>
+                </div>
+
                 <p className="text-center md:text-left italic opacity-80">{t("App-Banner-Slogan")}</p>
+                
             </div>
         </div>
     )
--- src/components/StartShiftCTA.jsx
+++ src/components/StartShiftCTA.jsx
@@ -4,7 +4,9 @@ import { useTranslation } from 'react-i18next';
 import Button from './common/Button'
 
 import { buttonTypes } from './common/Button'
-import { BoltIcon, CubeTransparentIcon } from '@heroicons/react/20/solid'
+import { BoltIcon } from '@heroicons/react/20/solid'
+
+import Logo from '../assets/svg/logo.svg?react'
 
 export const startGameButtonTestId = "startGameButtonTestId"
 
@@ -14,7 +16,7 @@ function StartShiftCTA(props) {
     return (
         <div className="w-full py-5 flex justify-between mb-2 gap-2 md:flex-row flex-col items-center border-2 border-solid border-color-back rounded px-5">
             <div className="flex items-center">
-                <CubeTransparentIcon className="large-icon mr-5" />
+                <Logo className="h-[3rem] w-fit mr-2 start-cta-logo" />
                 <h2 className="mb-0">{t("Start-Game-CTA-Header")}</h2>
             </div>
             <Button
